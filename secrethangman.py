f1a8557be59a6a38b6fc7afbb76ae189eb4a9734
rshan20 <60886351+rshan20@users.noreply.github.com>
2020-03-03 09:36:10 -0500

    Update secrethangman.py

--- secrethangman.py
+++ secrethangman.py
@@ -21,7 +21,21 @@ def main():
     getLetter()
     ifWon()
 main()
-
+val=0
+def test():
+    global val
+    secret = "BALOON"
+    updatedword = ['_','_','_','_','_','_','_']
+    for i in range (0, len(Secret)):
+        print (updatedWord[1]
+    Letter = str(input("What is your guess: "))
+    val = Secret.find (Letter)
+    del updatedWord[val]
+    updatedWord.insert(val, Letter)
+    for i in range (0, len(Secret)):
+            print(updatedWord[i], end =" ")
+    if Secret.find(Letter) ==-1
+        return "Wrong Letter"            
 #def tester():
     #global updateWord
     #updateWord = raw_input()
